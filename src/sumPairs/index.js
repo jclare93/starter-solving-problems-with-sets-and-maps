19f115d2dbec9d3e624cba5f2842a305659758bb
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-11-11 07:38:13 -0800

    find solutions

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,21 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+    for (let e of A){
+        numbers.set(e)
+    }
+    for (let e of A){
+        const diff = N-e
+        if (diff !== e){
+            if (numbers.has(diff)){
+                solution.set(Math.min[e, diff], Math.max[e, diff])
+            }
+        }
+    }
+    return Array.from(solution)
+}
 
 module.exports = sumPairs;
